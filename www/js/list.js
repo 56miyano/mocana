76bf257034283d9081b65235cdedd14e020e7738
56miyano <bjgm1290561@gn.iwasaki.ac.jp>
2020-02-03 10:20:06 +0900

    0203

--- www/js/list.js
+++ www/js/list.js
@@ -1,12 +1,21 @@
 // This is a JavaScript file
 const listArrange = () => {
-  console.log(photolist);
+  const list_cloth = document.getElementById("list_cloth");
   document.getElementById("list_cloth").innerHTML = "";
   for (let i= 0;i<photolist.length;i++){
-    const list_cloth = document.getElementById("list_cloth");
-    console.log(photolist);
     const new_cloth = document.createElement('li');
     new_cloth.innerHTML = '<img src="' + photolist[i] + '"class="list_img">';
     list_cloth.appendChild(new_cloth);
   }
+
+  const addButton = document.createElement('li');
+  addButton.id ="addButton";
+  addButton.innerHTML = 
+  '<img src="' + '../img/icon/plus.png"'+ 'width="100vw"' +'style="' + 'background-color: #ddddff"' + "; >"
+  +  "<br>服を追加";
+  list_cloth.appendChild(addButton);
+}
+
+const addCloth = () => {
+    
 }
