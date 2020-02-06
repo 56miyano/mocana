3e0b4e75967b6c850a0733e6d357ebff07edbaea
56miyano <bjgm1290561@gn.iwasaki.ac.jp>
2020-02-06 11:10:13 +0900

    model

--- www/js/list.js
+++ www/js/list.js
@@ -3,8 +3,10 @@ const listArrange = () => {
   const list_cloth = document.getElementById("list_cloth");
   document.getElementById("list_cloth").innerHTML = "";
   for (let i= 0;i<photolist.length;i++){
-    const new_cloth = document.createElement('li');
-    new_cloth.innerHTML = '<img src="' + photolist[i] + '"class="list_img">';
+    const new_cloth = document.createElement('a');
+    new_cloth.href = "#modal01";
+    new_cloth.className = "list_modal_open";
+    new_cloth.innerHTML = '<li>'+'<img src="' + photolist[i] + '"class="list_img">'+'</li>';
     list_cloth.appendChild(new_cloth);
   }
 
@@ -17,6 +19,28 @@ const listArrange = () => {
   addButton.addEventListener('touchend',function() {transition(3)},'false');
   list_cloth.appendChild(addButton);
 }
+
+$(function(){
+ //$(document).on("click", ".add-btn", function () {
+// モーダルウィンドウが開くときの処理    
+$(document).on("click",".list_modal_open",function(){
+      console.log("雨が躍るバスストップ");
+    var navClass = $(this).attr("class"),
+        href = $(this).attr("href");
+            
+        $(href).fadeIn();
+    $(this).addClass("open");
+    return false;
+});
+ 
+// モーダルウィンドウが閉じるときの処理    
+$(".modalClose").click(function(){
+    $(this).parents(".modal").fadeOut();
+    $(".modalOpen").removeClass("open");
+    return false;
+});  
+    
+});
 
 const addCloth = () => {
     
